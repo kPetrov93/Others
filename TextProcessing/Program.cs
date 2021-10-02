83b7adead5a6110124b0c17292e17832319c1d70
Kristian Petrov <83224475+kPetrov93@users.noreply.github.com>
2021-10-02 10:36:28 +0300

    Add files via upload

---
+++ TextProcessing/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Text;
+
+namespace TextProcessing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+          
+
+            while (input != "end")
+            {
+                string reversed = "";
+                for (int i = input.Length - 1; i >= 0; i--)
+                {
+                    reversed += input[i];
+
+                }
+                Console.WriteLine($"{input} = {reversed}");
+                input = Console.ReadLine();
+            }
+            
+        }
+    }
+}
---
+++ While/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace While
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            int c = int.Parse(Console.ReadLine());
+            double area = a * b * c;
+            double total = 0;
+
+            while (true)
+            {
+                string input = Console.ReadLine();
+                if (input == "Done")
+                {
+                    break;
+                }
+                double n = double.Parse(input);
+                total += n;
+                if (area - total <= 0)
+                {
+                    break;
+                }
+            }
+            if (area >= total)
+            {
+                Console.WriteLine($"{area - total} Cubic meters left.");
+            }
+            else
+            {
+                Console.WriteLine($"No more free space! You need {total-area} Cubic meters more.");
+            }
+
+        }
+    }
+}
