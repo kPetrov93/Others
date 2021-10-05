c02f62d90a9e4fb65fe9eeee227ac2d0df5a2a3d
Kristian Petrov <83224475+kPetrov93@users.noreply.github.com>
2021-10-05 11:17:24 +0300

    Add files via upload

---
+++ AssociativeArrays/5.WordFilter/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+namespace _5.WordFilter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split().Where(w=>w.Length%2==0).ToArray();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                Console.WriteLine(input[i]);
+            }
+        }
+    }
+}
