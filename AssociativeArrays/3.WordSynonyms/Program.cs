29c14e58481db305ff23e13c6d0da748309c1b28
Kristian Petrov <83224475+kPetrov93@users.noreply.github.com>
2021-10-05 11:14:23 +0300

    Add files via upload

---
+++ AssociativeArrays/3.WordSynonyms/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _3.WordSynonyms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            var synonymsDict = new Dictionary<string, List<string>>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string word = Console.ReadLine();
+                string synonym = Console.ReadLine();
+
+                if (!synonymsDict.ContainsKey(word))
+                {
+                    synonymsDict.Add(word, new List<string>());
+                }
+                synonymsDict[word].Add(synonym);
+            }
+
+            foreach (var item in synonymsDict)
+            {
+                Console.WriteLine($"{item.Key} - {string.Join(", ",item.Value)}");
+            }
+        }
+    }
+}
