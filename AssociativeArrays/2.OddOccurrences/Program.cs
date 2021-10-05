16dc79b1dac748b412f9c298fd8ea2a6da9dad8a
Kristian Petrov <83224475+kPetrov93@users.noreply.github.com>
2021-10-05 11:14:05 +0300

    Add files via upload

---
+++ AssociativeArrays/2.OddOccurrences/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _2.OddOccurrences
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] words = Console.ReadLine().ToLower().Split().ToArray();
+
+            Dictionary<string, int> input = new Dictionary<string, int>();
+
+            foreach (var word in words)
+            {
+                if (input.ContainsKey(word))
+                {
+                    input[word]++;
+                }
+                else
+                {
+                    input.Add(word, 1);
+                }
+            }
+            foreach (var word in input)
+            {
+                if (word.Value%2==1)
+                {
+                    Console.Write($"{word.Key} ");
+                }
+            }
+        }
+    }
+}
