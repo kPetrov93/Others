0801d0c70765dffef3c264e2b720afc7df4c9690
Kristian Petrov <83224475+kPetrov93@users.noreply.github.com>
2021-10-05 11:14:59 +0300

    Add files via upload

---
+++ AssociativeArrays/4.Largest3Numbers/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+
+namespace _4.Largest3Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] num = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            int[] sorted = num.OrderByDescending(n => n).ToArray();
+
+            if (num.Length >= 3)
+            {
+                for (int i = 0; i < 3; i++)
+                {
+                    Console.Write(sorted[i] + " ");
+                }
+            }
+            else
+            {
+                Console.WriteLine(string.Join(" ",sorted));
+            }
+        }
+    }
+}
