15483ad33f1b8af7ff93e1f53c6684491637c28e
Kristian Petrov <83224475+kPetrov93@users.noreply.github.com>
2021-10-06 09:44:47 +0300

    Add files via upload

---
+++ Exercise03/Alcohole/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Alcohole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double priceWiskey = double.Parse(Console.ReadLine());
+            double beer = double.Parse(Console.ReadLine());
+            double wine = double.Parse(Console.ReadLine());
+            double rakia = double.Parse(Console.ReadLine());
+            double wiskey = double.Parse(Console.ReadLine());
+
+            double priceRakia = priceWiskey * 0.5;
+            double priceWine = priceRakia - (priceRakia * 0.4);
+            double priceBeer = priceRakia - (priceRakia * 0.8);
+            double total = (priceWiskey*wiskey) + (priceWine*wine) + (priceRakia*rakia) + (priceBeer*beer);
+
+            Console.WriteLine($"{total:f2}");
+
+        }
+    }
+}
