b578f0d35bba2c51480891141c00be4542d540ab
Kristian Petrov <83224475+kPetrov93@users.noreply.github.com>
2021-10-06 09:46:44 +0300

    Add files via upload

---
+++ Exercise03/MakingProject/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace MakingProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            int project = int.Parse(Console.ReadLine());
+
+            int time = project * 3;
+
+            Console.WriteLine($"The architect {name} will need {time} hours to complete {project} project/s.");
+
+
+        }
+    }
+}
---
+++ Exercise03/PetShopp/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+
+class Program
+{
+    static void Main()
+    {
+        int dogs = int.Parse(Console.ReadLine());
+        int others = int.Parse(Console.ReadLine());
+
+        double dogsFood = dogs * 2.50;
+        int othersFood = others * 4;
+        double total = dogsFood + othersFood;
+
+        Console.WriteLine($"{total:f2} lv.");
+    }
+}
