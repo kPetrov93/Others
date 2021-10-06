1d13c82794db37a12ea4a015e833780ddd56f947
Kristian Petrov <83224475+kPetrov93@users.noreply.github.com>
2021-10-06 09:45:51 +0300

    Add files via upload

---
+++ Exercise03/Aquarium/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Aquarium
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int lenght = int.Parse(Console.ReadLine());
+            int width = int.Parse(Console.ReadLine());
+            int hight = int.Parse(Console.ReadLine());
+            double percent = double.Parse(Console.ReadLine());
+
+            double size = lenght * width * hight;
+            double litres = size * 0.001;
+            double percentage = percent * 0.01;
+            double total = litres * (1 - percentage);
+
+            Console.WriteLine($"{total:f3}");
+
+        }
+    }
+}
