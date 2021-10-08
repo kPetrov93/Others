da860619d74a48aaab3c758881bfc1e38cc9eea1
Kristian Petrov <83224475+kPetrov93@users.noreply.github.com>
2021-10-08 13:54:50 +0300

    Add files via upload

---
+++ DataTypesAndVariablesLab/DataTypesAndVariablesLab/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace DataTypesAndVariablesLab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            string biggestKeg = string.Empty;
+            double biggestVolume = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                string name = Console.ReadLine();
+                double r = double.Parse(Console.ReadLine());
+                double h = double.Parse(Console.ReadLine());
+
+                double volume = Math.PI * (r * r) * h;
+
+                if (volume>biggestVolume)
+                {
+                    biggestKeg = name;
+                    biggestVolume = volume;
+                }
+            }
+            Console.WriteLine(biggestKeg);
+
+        }
+    }
+}
