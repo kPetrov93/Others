1d0074275430250116b3099d3a70b40a6542e0d0
Kristian Petrov <83224475+kPetrov93@users.noreply.github.com>
2021-10-08 13:57:03 +0300

    Add files via upload

---
+++ DataTypesAndVariablesLab/PokeMon/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace PokeMon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int m = int.Parse(Console.ReadLine());
+            int y = int.Parse(Console.ReadLine());
+            
+            int startN = n;
+            int pokes = 0;
+
+            while (n>=m)
+            {
+                n -= m;
+                pokes++;
+                if (n==startN/2&&y>0&&y<=n)
+                {
+                    n /= y;
+                }
+            }
+
+            Console.WriteLine(n);
+            Console.WriteLine(pokes);
+
+        }
+    }
+}
