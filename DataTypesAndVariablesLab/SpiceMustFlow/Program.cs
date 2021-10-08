6780d3f8028323be9fa110e99703f90e875696cd
Kristian Petrov <83224475+kPetrov93@users.noreply.github.com>
2021-10-08 13:55:50 +0300

    Add files via upload

---
+++ DataTypesAndVariablesLab/SpiceMustFlow/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace SpiceMustFlow
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int yield = int.Parse(Console.ReadLine());
+            int total = 0;
+            int days = 0;
+
+            while (yield >= 100)
+            {
+                total += yield;
+                if (total > 26)
+                {
+                    total -= 26;
+                }
+                days++;
+                yield -= 10;
+            }
+
+            if (total>26)
+            {
+                total -= 26;
+            }
+
+            Console.WriteLine(days);
+            Console.WriteLine(total);
+        }
+    }
+}
