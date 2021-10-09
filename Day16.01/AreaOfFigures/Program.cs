302cb8c09bd4b20aae038819e8a0ef251fd0c466
Kristian Petrov <83224475+kPetrov93@users.noreply.github.com>
2021-10-09 11:27:54 +0300

    Add files via upload

---
+++ Day16.01/AreaOfFigures/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace AreaOfFigures
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string figure = Console.ReadLine();
+            if (figure == "square")
+            {
+                double side = double.Parse(Console.ReadLine());
+                Console.WriteLine($"{side * side:f3}");
+            }
+            else if (figure == "rectangle")
+            {
+                double sideA = double.Parse(Console.ReadLine());
+                double sideB = double.Parse(Console.ReadLine());
+                Console.WriteLine($"{sideA * sideB:f3}");
+            }
+            else if (figure=="circle")
+            {
+                double r = double.Parse(Console.ReadLine());
+                Console.WriteLine($"{Math.PI *r*r:f3}");
+            }
+            else if (figure=="triangle")
+            {
+                double sideA = double.Parse(Console.ReadLine());
+                double sideB = double.Parse(Console.ReadLine());
+                Console.WriteLine($"{(sideA * sideB)/2:f3}");
+            }
+        }
+    }
+}
