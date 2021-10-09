17e68d81f6dc6053f512e462452e9ffe824be674
Kristian Petrov <83224475+kPetrov93@users.noreply.github.com>
2021-10-09 11:27:25 +0300

    Add files via upload

---
+++ Day16.01/Day16.01/ExcellentResult.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Day16._01
+{
+    class ExcellentResult
+    {
+        static void Main(string[] args)
+        {
+            double mark = double.Parse(Console.ReadLine());
+            if (mark>=5.50)
+            {
+                Console.WriteLine("Excellent!");
+            }
+        }
+    }
+}
