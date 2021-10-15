65f0c7a0602ba13a16ace32c08471617557e1b59
Kristian Petrov <83224475+kPetrov93@users.noreply.github.com>
2021-10-15 10:17:46 +0300

    Add files via upload

---
+++ Day01.17/MetricConverter/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace MetricConverter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double num = double.Parse(Console.ReadLine());
+            string input = Console.ReadLine();
+            string output = Console.ReadLine();
+
+            if (input == "m")
+            {
+                if (output == "cm")
+                {
+                    Console.WriteLine($"{num * 100:f3}");
+                }
+                else if (output == "mm")
+                {
+                    Console.WriteLine($"{num * 1000:f3}");
+                }
+            }
+            else if (input == "cm")
+            {
+                if (output == "m")
+                {
+                    Console.WriteLine($"{num / 100:f3}");
+                }
+                else if (output == "mm")
+                {
+                    Console.WriteLine($"{num * 10:f3}");
+                }
+            }
+            else if (input == "mm")
+
+
+            {
+                if (output == "cm")
+                {
+                    Console.WriteLine($"{num / 10:f3}");
+                }
+                else if (output == "m")
+                {
+                    Console.WriteLine($"{num / 1000:f3}");
+                }
+            }
+                }
+    }
+}
