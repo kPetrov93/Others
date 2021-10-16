821d6581f247e7bc3b329f6b66f55337776fc9d9
Kristian Petrov <83224475+kPetrov93@users.noreply.github.com>
2021-10-16 10:50:13 +0300

    Add files via upload

---
+++ Day01.17/WorldSwimmingRecord/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace WorldSwimmingRecord
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double record = double.Parse(Console.ReadLine());
+            double distance = double.Parse(Console.ReadLine());
+            double tpm = double.Parse(Console.ReadLine());
+
+            double slow =Math.Floor (distance / 15);
+            double drag = slow * 12.5;
+            double time = (distance * tpm) + drag;
+            if (time < record)
+            {
+                Console.WriteLine($"Yes, he succeeded! The new world record is {time:f2} seconds.");
+            }
+            else
+            {
+                Console.WriteLine($"No, he failed! He was {time-record:f2} seconds slower.");
+            }
+
+
+        }
+    }
+}
