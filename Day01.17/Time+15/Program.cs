da11f67fb920e479f7f81afcdc4546ce75442bdd
Kristian Petrov <83224475+kPetrov93@users.noreply.github.com>
2021-10-16 10:49:52 +0300

    Add files via upload

---
+++ Day01.17/Time+15/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Time_15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int hours = int.Parse(Console.ReadLine());
+            int minutes = int.Parse(Console.ReadLine());
+
+            int min = minutes + 15;
+            if (min >= 60)
+            {
+                hours = hours + 1;
+                if (hours >= 24)
+                {
+                    hours = hours - 24;
+                    min = min - 60;
+                    Console.WriteLine($"{hours}:{min:d2}");
+                }
+                else
+                {
+                    min = min - 60;
+                    Console.WriteLine($"{hours}:{min:d2}");
+                }
+            }
+            else
+            {
+
+                if (hours >= 24)
+                {
+                    hours = hours - 24;
+                    min = min - 60;
+                    Console.WriteLine($"{hours}:{min:d2}");
+                }
+                else if (min<=60)
+                {
+                    Console.WriteLine($"{hours}:{min:d2}");
+                }
+                else
+                {
+                    min = min - 60;
+                    Console.WriteLine($"{hours}:{min:d2}");
+                }
+            }
+        }
+    }
+}
