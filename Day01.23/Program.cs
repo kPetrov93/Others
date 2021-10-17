512ee5420580333cc99c0bc61ce280c393f34b9c
Kristian Petrov <83224475+kPetrov93@users.noreply.github.com>
2021-10-17 16:21:55 +0300

    Add files via upload

---
+++ Day01.23/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Day01._23
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string year = Console.ReadLine();
+            double p = double.Parse(Console.ReadLine());
+            double h = double.Parse(Console.ReadLine());
+            double weekends = 48;
+            double backHome = weekends - h;
+            double sofia = backHome * 3 / 4;
+            double celeb = p * 2 / 3;
+            double total =sofia + h + celeb;
+
+            if (year=="leap")
+            {
+                total =Math.Floor(total * 1.15);
+                Console.WriteLine(total);  
+            }
+            else
+            {
+                total = Math.Floor(total);
+                Console.WriteLine(total);
+            }
+
+        }
+    }
+}
