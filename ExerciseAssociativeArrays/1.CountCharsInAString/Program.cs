986adba946ba207629f24358058e3f4745fea84e
Kristian Petrov <83224475+kPetrov93@users.noreply.github.com>
2021-10-19 11:12:58 +0300

    Add files via upload

---
+++ ExerciseAssociativeArrays/1.CountCharsInAString/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _1.CountCharsInAString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split().ToArray();
+
+            Dictionary<char, int> dict = new Dictionary<char, int>();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                for (int j = 0; j < input[i].Length; j++)
+                {
+                    string currentWord = input[i];
+                    char currentLetter = currentWord[j];
+
+                    if (!dict.ContainsKey(currentLetter))
+                    {
+                        dict.Add(currentLetter, 0);
+                    }
+                    dict[currentLetter]++;
+                }
+            }
+            foreach (var item in dict)
+            {
+                Console.WriteLine($"{item.Key} -> {item.Value}");
+            }
+        }
+    }
+}
