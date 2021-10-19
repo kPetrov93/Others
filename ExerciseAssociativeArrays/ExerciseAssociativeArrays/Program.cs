b1a076491cc5fc1e19b16d57fa41358f67ca0d27
Kristian Petrov <83224475+kPetrov93@users.noreply.github.com>
2021-10-19 11:12:26 +0300

    Add files via upload

---
+++ ExerciseAssociativeArrays/ExerciseAssociativeArrays/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ExerciseAssociativeArrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            Dictionary<string, int> dict = new Dictionary<string, int>();
+
+            while (input != "stop")
+            {
+                int value = int.Parse(Console.ReadLine());
+
+                if (!dict.ContainsKey(input))
+                {
+                    dict.Add(input, 0);
+                }
+                dict[input] += value;
+
+                input = Console.ReadLine();
+            }
+
+            foreach (var item in dict)
+            {
+                Console.WriteLine($"{item.Key} -> {item.Value}");
+            }
+        }
+    }
+}
