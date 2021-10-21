61e28ccbf9dc4f9881f1dd76a9704387453f7d60
Kristian Petrov <83224475+kPetrov93@users.noreply.github.com>
2021-10-21 10:24:53 +0300

    Add files via upload

---
+++ ExerciseAssociativeArrays/1414/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _1414
+{
+    class Program
+    {
+        public static object Dictonary { get; private set; }
+
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().ToLower().Split().ToArray();
+
+            Dictionary<string, int> sorted = new Dictionary<string, int>();
+
+            foreach (var item in input)
+            {
+                if (!sorted.ContainsKey(item))
+                {
+                    sorted.Add(item, 0);
+                }
+                sorted[item]++;
+            }
+
+            foreach (var item in sorted)
+            {
+                if (item.Value%2==1)
+                {
+                    Console.Write($"{item.Key} ");
+                }
+            }
+        }
+    }
+}
