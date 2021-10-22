805dd72f9bef9cf7dd9983855a0b3af33bfe4d0c
Kristian Petrov <83224475+kPetrov93@users.noreply.github.com>
2021-10-22 10:59:46 +0300

    Add files via upload

---
+++ ExamPrep/02.Shopping/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace _02.Shopping
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double budget = double.Parse(Console.ReadLine());
+            double videoCards = double.Parse(Console.ReadLine());
+            double processors = double.Parse(Console.ReadLine());
+            double ram = double.Parse(Console.ReadLine());
+
+            double priceVideo = 250 * videoCards;
+            double priceProcess = (priceVideo * 0.35)*processors;
+            double priceRam = (priceVideo * 0.10)*ram;
+            double total = priceVideo + priceRam + priceProcess;
+
+            if (videoCards > processors)
+            {
+                total = total * 0.85;
+            }
+
+            if (total<=budget)
+            {
+                Console.WriteLine($"You have {budget-total:f2} leva left!");
+            }
+            else
+            {
+                Console.WriteLine($"Not enough money! You need {total-budget:f2} leva more!");
+            }
+        }
+    }
+}
