837d9acd096edb677c29750147439f5c28043827
Kristian Petrov <83224475+kPetrov93@users.noreply.github.com>
2021-10-22 10:59:10 +0300

    Add files via upload

---
+++ ExamPrep/01.PoolDay/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ExamPrep
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double people = double.Parse(Console.ReadLine());
+            double entranceFee = double.Parse(Console.ReadLine());
+            double bedPrice = double.Parse(Console.ReadLine());
+            double umbrellaPrice = double.Parse(Console.ReadLine());
+
+            double entranceIncome = people * entranceFee;
+            double bedIncome = (Math.Ceiling(people * 0.75)) * bedPrice;
+            double umbrellaIncome = (Math.Ceiling(people * 0.50)) * umbrellaPrice;
+            double total = entranceIncome + bedIncome + umbrellaIncome;
+
+            Console.WriteLine($"{total:f2} lv.");
+
+        }
+    }
+}
