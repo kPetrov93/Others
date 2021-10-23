ed114e0c75f9a57f01898e6264b65afe268c05ef
Kristian Petrov <83224475+kPetrov93@users.noreply.github.com>
2021-10-23 10:14:22 +0300

    Add files via upload

---
+++ Lecture1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Lecture1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            string family = Console.ReadLine();
+            int age = int.Parse(Console.ReadLine());
+            string town = Console.ReadLine();
+
+            Console.WriteLine($"You are {name} {family}, {age}-years old person from {town}.");
+        }
+    }
+}
