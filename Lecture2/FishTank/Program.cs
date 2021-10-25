a266f71db91df49bd428ed1f1daf214030b42725
Kristian Petrov <83224475+kPetrov93@users.noreply.github.com>
2021-10-25 12:10:14 +0300

    Add files via upload

---
+++ Lecture2/FishTank/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace FishTank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int lenght = int.Parse(Console.ReadLine());
+            int wight = int.Parse(Console.ReadLine());
+            int hight = int.Parse(Console.ReadLine());
+            double percent = double.Parse(Console.ReadLine());
+
+            double area = (lenght * wight * hight) * 0.001;
+            double tPercent = percent * 0.01;
+            double total = area - (area * tPercent);
+            Console.WriteLine(total);
+        }
+    }
+}
