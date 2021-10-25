336836875d5cd85e35dfaadb11ee581c2cae317d
Kristian Petrov <83224475+kPetrov93@users.noreply.github.com>
2021-10-25 12:10:28 +0300

    Add files via upload

---
+++ Lecture2/FruitMarket/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace FruitMarket
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double strawberriesP = double.Parse(Console.ReadLine());
+            double bananas = double.Parse(Console.ReadLine());
+            double oranges = double.Parse(Console.ReadLine());
+            double raspberries = double.Parse(Console.ReadLine());
+            double strawberries = double.Parse(Console.ReadLine());
+
+            double raspberriesP = strawberriesP / 2;
+            double orangesP = raspberriesP * 0.60;
+            double bananasP = raspberriesP * 0.20;
+
+            double total = strawberries * strawberriesP + bananas * bananasP + oranges * orangesP + raspberries * raspberriesP;
+
+            Console.WriteLine($"{total:f2}");
+
+
+
+        }
+    }
+}
