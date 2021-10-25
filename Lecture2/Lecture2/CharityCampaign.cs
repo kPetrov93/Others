294fbd1fea9b793e9f153a34971ee2b2e687c685
Kristian Petrov <83224475+kPetrov93@users.noreply.github.com>
2021-10-25 12:09:45 +0300

    Add files via upload

---
+++ Lecture2/Lecture2/CharityCampaign.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Lecture2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int days = int.Parse(Console.ReadLine());
+            int cooks = int.Parse(Console.ReadLine());
+            int cakes = int.Parse(Console.ReadLine());
+            int waffles = int.Parse(Console.ReadLine());
+            int pancakes = int.Parse(Console.ReadLine());
+
+            double cakesP = cakes * 45;
+            double wafflesP = waffles * 5.80;
+            double pancakesP = pancakes * 3.20;
+
+            double total = days * (cooks * (cakesP + wafflesP + pancakesP));
+            double discount = total / 8;
+            Console.WriteLine(total-discount);
+
+
+        }
+    }
+}
