2a87ba6cabed24cc4849367d6cc9c2b1c7cd2413
Kristian Petrov <83224475+kPetrov93@users.noreply.github.com>
2021-10-26 12:59:23 +0300

    Add files via upload

---
+++ lecture8/Schoolarship/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace Schoolarship
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double income = double.Parse(Console.ReadLine());
+            double marks = double.Parse(Console.ReadLine());
+            double wage = double.Parse(Console.ReadLine());
+
+            double social = Math.Floor(wage * 0.35);
+            double excellent =Math.Floor(marks * 25);
+
+            if (income >= wage)
+            {
+                if (marks < 5.5)
+                {
+                    Console.WriteLine("You cannot get a scholarship!");
+                }
+                else if (marks >= 5.5)
+                {
+                    Console.WriteLine($"You get a scholarship for excellent results {excellent} BGN");
+                }
+            }
+            else if (income <= wage)
+            {
+                if (marks >= 5.5)
+                {
+                    if (excellent >= social)
+                    {
+                        Console.WriteLine($"You get a scholarship for excellent results {excellent} BGN");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"You get a Social scholarship {social} BGN");
+                    }
+                }
+                else if (marks > 4.5)
+                {
+                    Console.WriteLine($"You get a Social scholarship {social} BGN");
+                }
+                else
+                {
+                    Console.WriteLine("You cannot get a scholarship!");
+                }
+            }
+        }
+    }
+}
