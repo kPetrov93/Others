de6b282cfa7c2dcfd45e3cbcad1b73b04c2b2fab
Kristian Petrov <83224475+kPetrov93@users.noreply.github.com>
2021-10-26 12:58:24 +0300

    Add files via upload

---
+++ lecture8/lecture8/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace lecture8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double value = double.Parse(Console.ReadLine());
+            string input = Console.ReadLine();
+            string output = Console.ReadLine();
+
+            if (input == "mm")
+            {
+                if (output == "m")
+                {
+                    Console.WriteLine($"{value / 1000:f3}");
+                }
+
+                else if (output == "cm")
+                {
+                    Console.WriteLine($"{value / 10:f3}");
+                }
+            }
+
+            else if (input == "m")
+            {
+                if (output == "mm")
+                {
+                    Console.WriteLine($"{value *1000:f3}");
+                }
+                else if (output == "cm")
+                {
+                    Console.WriteLine($"{value *100:f3}");
+                }
+            }
+
+            else if (input == "cm")
+            {
+                if (output == "mm")
+                {
+                    Console.WriteLine($"{value * 10:f3}");
+                }
+                else if (output == "m")
+                {
+                    Console.WriteLine($"{value /100:f3}");
+                }
+            }
+        }
+    }
+}
