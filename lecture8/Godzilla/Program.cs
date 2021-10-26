8db392cbfe1360d1324cb42dee8a10c18f0d7819
Kristian Petrov <83224475+kPetrov93@users.noreply.github.com>
2021-10-26 12:59:12 +0300

    Add files via upload

---
+++ lecture8/Godzilla/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Godzilla
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double budget = double.Parse(Console.ReadLine());
+            double stats = double.Parse(Console.ReadLine());
+            double clothsPrice = double.Parse(Console.ReadLine());
+
+            double decor = budget * 0.1;
+            if (stats > 150)
+            {
+                clothsPrice = clothsPrice - (clothsPrice * 0.1);
+            }
+
+            double leftover = Math.Abs(budget - ((stats * clothsPrice) + decor));
+
+
+
+            if (budget >= ((stats * clothsPrice) + decor))
+
+            {
+                Console.WriteLine("Action!");
+                Console.WriteLine($"Wingard starts filming with {leftover:f2} leva left.");
+            }
+            
+            else 
+            {
+                
+                Console.WriteLine("Not enough money!");
+                Console.WriteLine($"Wingard needs {leftover:f2} leva more.");
+            }
+
+
+
+
+        }
+    }
+}
