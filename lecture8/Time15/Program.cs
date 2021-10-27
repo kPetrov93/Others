061e9b23186c3b8f35ce7951494f6d180fc7ce4e
Kristian Petrov <83224475+kPetrov93@users.noreply.github.com>
2021-10-27 13:53:35 +0300

    Add files via upload

---
+++ lecture8/Time15/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Time15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int hours = int.Parse(Console.ReadLine());
+            int minutes = int.Parse(Console.ReadLine());
+
+            int totalM = minutes + 15;
+
+            if (totalM >= 60)
+            {
+                hours = hours + 1;
+                totalM = totalM % 60;
+                if (hours >= 24)
+                {
+                    hours = hours % 24;
+                    totalM = totalM % 60;
+                }
+            }
+            
+            Console.WriteLine($"{hours}:{totalM:d2}");
+
+       }
+    }
+}
