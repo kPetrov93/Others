6660d1a4bb4d4557ed223206f1008efa20175acd
Kristian Petrov <83224475+kPetrov93@users.noreply.github.com>
2021-10-30 11:37:54 +0300

    Add files via upload

---
+++ Lecture9/InvNum/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace InvNum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+
+            if (num >=100 && num <= 200 || num == 0)
+            {
+                Console.WriteLine("");
+            }
+            else
+            {
+                Console.WriteLine("invalid");
+            }
+
+
+        }
+    }
+}
