0dc98f65920eef6cf3c0a8c31f3db4f5811a561f
Kristian Petrov <83224475+kPetrov93@users.noreply.github.com>
2021-10-31 10:43:35 +0200

    Add files via upload

---
+++ Lecture9/PersonalTitles/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace PersonalTitles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double age = double.Parse(Console.ReadLine());
+            string gen = Console.ReadLine();
+
+            if (age >= 16)
+            {
+                if (gen == "m")
+                {
+                    Console.WriteLine("Mr.");
+                }
+
+
+                else if (gen == "f")
+                {
+                    Console.WriteLine("Ms.");
+                }
+            }
+            else 
+            {
+                if (gen == "m")
+                {
+                    Console.WriteLine("Master");
+                }
+                else if (gen == "f")
+                {
+                    Console.WriteLine("Miss");
+                }
+            }
+        }
+    }
+}
