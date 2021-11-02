e78a5fcc0a10f6a0544e362642c965bca440f62a
Kristian Petrov <83224475+kPetrov93@users.noreply.github.com>
2021-11-02 09:43:36 +0200

    Add files via upload

---
+++ Lecture10/Cinema/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Cinema
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string proj = Console.ReadLine();
+            int r = int.Parse(Console.ReadLine());
+            int c = int.Parse(Console.ReadLine());
+            double ticket = 0;
+            if (proj == "Premiere")
+            {
+                ticket = 12;
+            }
+            else if (proj=="Normal")
+            {
+                ticket = 7.5;
+            }
+            else if (proj=="Discount")
+            {
+                ticket = 5;
+            }
+            Console.WriteLine($"{ticket*r*c:f2} leva");
+        }
+    }
+}
