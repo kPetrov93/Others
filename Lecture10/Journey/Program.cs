05708745bf150e2146cbb908091417da2b251861
Kristian Petrov <83224475+kPetrov93@users.noreply.github.com>
2021-11-03 10:54:48 +0200

    Add files via upload

---
+++ Lecture10/Journey/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace Journey
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double budget = double.Parse(Console.ReadLine());
+            string season = Console.ReadLine();
+            string destination = "";
+            double price = 0;
+            string place = "";
+            if (season=="summer")
+            {
+                place = "Camp";
+                if (budget <= 100)
+                {
+                    destination = "Bulgaria";
+                    price = budget * 0.30;
+                }
+                else if (budget<=1000)
+                {
+                    destination = "Balkans";
+                    price = budget * 0.40;
+                }
+                else
+                {
+                    destination = "Europe";
+                    price = budget * 0.9;
+                    place = "Hotel";
+                }
+            }
+            else if (season=="winter")
+            {
+                place = "Hotel";
+                if (budget <= 100)
+                {
+                    destination = "Bulgaria";
+                    price = budget * 0.70;
+                }
+                else if (budget <= 1000)
+                {
+                    destination = "Balkans";
+                    price = budget * 0.80;
+                }
+                else
+                {
+                    destination = "Europe";
+                    price = budget * 0.9;
+                    place = "Hotel";
+                }
+            }
+            Console.WriteLine($"Somewhere in {destination}");
+            Console.WriteLine($"{place} - {price:f2}");
+        }
+    }
+}
