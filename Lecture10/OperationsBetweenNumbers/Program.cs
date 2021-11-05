942b90ce3f7a0b61e656ba54003d9a3304d0f078
Kristian Petrov <83224475+kPetrov93@users.noreply.github.com>
2021-11-05 11:29:45 +0200

    Add files via upload

---
+++ Lecture10/OperationsBetweenNumbers/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace OperationsBetweenNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double n1 = double.Parse(Console.ReadLine());
+            double n2 = double.Parse(Console.ReadLine());
+            string oper = Console.ReadLine();
+            double result = 0;
+
+            if (oper == "+")
+            {
+                result = n1 + n2;
+            }
+            else if (oper == "-")
+            {
+                result = n1 - n2;
+            }
+            else if (oper == "*")
+            {
+                result = n1 * n2;
+            }
+            else if (oper == "/")
+            {
+                result = n1 / n2;
+            }
+            else if (oper == "%")
+            {
+                result = n1 % n2;
+            }
+            if (oper == "+" || oper == "-" || oper == "*")
+            {
+                if (result % 2 == 0)
+                {
+                    Console.WriteLine($"{n1} {oper} {n2} = {result} - even");
+                }
+                else
+                {
+                    Console.WriteLine($"{n1} {oper} {n2} = {result} - odd");
+                }
+            }
+            else if (oper == "/")
+            {
+                if (n2 == 0)
+                {
+                    Console.WriteLine($"Cannot divide {n1} by zero");
+                }
+                else
+                {
+                    Console.WriteLine($"{n1} / {n2} = {result:f2}");
+                }
+            }
+            else if (oper=="%")
+            {
+                if (n2 == 0)
+                {
+                    Console.WriteLine($"Cannot divide {n1} by zero");
+                }
+                else
+                {
+                    Console.WriteLine($"{n1} % {n2} = {result}");
+                }
+            }
+        }
+    }
+}
