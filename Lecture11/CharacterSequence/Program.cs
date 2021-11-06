422a5de6113de944f4c3be4ba46016cf9d387fe8
Kristian Petrov <83224475+kPetrov93@users.noreply.github.com>
2021-11-06 09:30:46 +0200

    Add files via upload

---
+++ Lecture11/CharacterSequence/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace CharacterSequence
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            int leng = input.Length;
+            
+            for (int i = 0; i < leng; i++)
+            {
+                Console.WriteLine(input[i]);
+            }
+        }
+    }
+}
