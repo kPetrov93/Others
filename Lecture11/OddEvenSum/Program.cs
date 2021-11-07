b721909a1bd498c48405a8a0af8b016e104c6d23
Kristian Petrov <83224475+kPetrov93@users.noreply.github.com>
2021-11-07 12:37:09 +0200

    Add files via upload

---
+++ Lecture11/OddEvenSum/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace OddEvenSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int odd = 0;
+            int even = 0;
+            for (int i = 0; i < n; i++)
+            {
+                int currentNum = int.Parse(Console.ReadLine());
+                if (i % 2 == 0)
+                {
+                    even += currentNum;
+                }
+                else
+                {
+                    odd += currentNum;
+                }
+            }
+            int diff = Math.Abs(odd - even);
+            if (odd==even)
+            {
+                Console.WriteLine("Yes");
+                Console.WriteLine($"Sum = {odd}");
+            }
+            else
+            {
+                Console.WriteLine("No");
+                Console.WriteLine($"Diff = {diff}");
+            }
+        }
+    }
+}
