65e9a2576c6c7017b38f3abd681773614b39f13d
Kristian Petrov <83224475+kPetrov93@users.noreply.github.com>
2021-11-07 12:36:49 +0200

    Add files via upload

---
+++ Lecture11/LeftAndRightSum/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace LeftAndRightSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = int.Parse(Console.ReadLine());
+            int left = 0;
+            int right = 0;
+            for (int i = 0; i < count; i++)
+            {
+                int leftN = int.Parse(Console.ReadLine());
+                left += leftN;
+            }
+            for (int i = 0; i < count; i++)
+            {
+                int rightN = int.Parse(Console.ReadLine());
+                right += rightN;
+            }
+            int all = Math.Abs(left - right);
+            if (left==right)
+            {
+                Console.WriteLine($"Yes, sum = {left}");
+            }
+            else
+            {
+                Console.WriteLine($"No, diff = {all}");
+            }
+        }
+    }
+}
