d0ebcd491ba6c09f97b533def30a5a453ef6d207
Kristian Petrov <83224475+kPetrov93@users.noreply.github.com>
2021-11-07 12:36:33 +0200

    Add files via upload

---
+++ Lecture11/Lecture11/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Lecture11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i <= n; i++)
+            {
+                if (i%2==0)
+                {
+                    double result = Math.Pow(2, i);
+                    Console.WriteLine(result);
+                }
+                
+            }
+
+        }
+    }
+}
