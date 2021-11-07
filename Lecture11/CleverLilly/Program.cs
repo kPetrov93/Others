ba98d53be1118c1d1e5cbffea3b4995a1cda9572
Kristian Petrov <83224475+kPetrov93@users.noreply.github.com>
2021-11-07 12:36:17 +0200

    Add files via upload

---
+++ Lecture11/CleverLilly/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace CleverLilly
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double n = double.Parse(Console.ReadLine());
+            double washingM = double.Parse(Console.ReadLine());
+            double toyP = double.Parse(Console.ReadLine());
+            double saved = 0;
+            double toy = 0;
+            double money = 10;
+            for (int i = 1 ; i <= n; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    saved += money;
+                    saved -= 1;
+                    money += 10;
+                }
+                else
+                {
+                    toy += 1;
+                }
+            }
+            double total = saved + (toy * toyP);
+            if (total>=washingM)
+            {
+                Console.WriteLine($"Yes! {total-washingM:f2}");
+            }
+            else
+            {
+                Console.WriteLine($"No! {washingM-total:f2}");
+            }
+        }
+    }
+}
