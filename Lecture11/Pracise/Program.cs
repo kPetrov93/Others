3456ebf39ee3ae367f596586c5dc50aff2d9a93d
Kristian Petrov <83224475+kPetrov93@users.noreply.github.com>
2021-11-08 10:02:10 +0200

    Add files via upload

---
+++ Lecture11/Pracise/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Pracise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int max = int.MinValue;
+            int min = int.MaxValue;
+            for (int i = 0; i < n; i++)
+            {
+                int num1 = int.Parse(Console.ReadLine());
+                if (num1>max)
+                {
+                    max = num1;
+                }
+                if (num1<min)
+                {
+                    min = num1;
+                }
+            }
+            Console.WriteLine($"Max number: {max}");
+            Console.WriteLine($"Min number: {min}");
+        }
+    }
+}
