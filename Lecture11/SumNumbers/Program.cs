1c6454cdd18200ca885ed4371b98e25a75cb63a8
Kristian Petrov <83224475+kPetrov93@users.noreply.github.com>
2021-11-08 10:02:27 +0200

    Add files via upload

---
+++ Lecture11/SumNumbers/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace SumNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int sum = 0;
+            for (int i = 0; i < n; i++)
+            {
+                int newN = int.Parse(Console.ReadLine());
+                sum += newN;
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
