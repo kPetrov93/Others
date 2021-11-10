2d26eef0b17e4dc951f0d0c6701f0b6402ae4641
Kristian Petrov <83224475+kPetrov93@users.noreply.github.com>
2021-11-10 14:00:41 +0200

    Add files via upload

---
+++ Lecture12/Lecture12/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Lecture12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int left = 0;
+            int right = 0;
+            for (int i = 0; i < n; i++)
+            {
+                int num = int.Parse(Console.ReadLine());
+                int num2 = int.Parse(Console.ReadLine());
+                if (i % 2 == 0)
+                {
+                    left += num;
+                    left += num2;
+                }
+                else
+                {
+                    right += num;
+                    right += num2;
+                }
+            }
+            double total = Math.Abs(left - right);
+            if (left == right)
+            {
+                Console.WriteLine($"Yes, sum = {right}");
+            }
+            else
+            {
+                Console.WriteLine($"No, diff = {total}");
+            }
+        }
+    }
+}
