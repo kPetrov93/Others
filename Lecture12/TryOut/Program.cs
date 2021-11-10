d244ebada5dad89cdb0d8a34f2281af61ddb0739
Kristian Petrov <83224475+kPetrov93@users.noreply.github.com>
2021-11-10 14:00:19 +0200

    Add files via upload

---
+++ Lecture12/TryOut/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace TryOut
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int salary = int.Parse(Console.ReadLine());
+            int fine = 0;
+
+
+            for (int i = 0; i < n; i++)
+            {
+                string site = Console.ReadLine();
+
+                if (site == "Facebook")
+                {
+                    fine += 150;
+                }
+                else if (site == "Instagram")
+                {
+                    fine += 100;
+                }
+                else if (site == "Reddit")
+                {
+                    fine += 50;
+                }
+            }
+            if (fine >= salary)
+            {
+                Console.WriteLine("You have lost your salary.");
+            }
+            else
+            {
+                Console.WriteLine(salary-fine);
+            }
+        }
+    }
+}
