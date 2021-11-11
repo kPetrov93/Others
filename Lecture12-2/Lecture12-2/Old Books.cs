69cbc03566fa7d5515f2cceb3d0d7f70b884424e
Kristian Petrov <83224475+kPetrov93@users.noreply.github.com>
2021-11-11 10:17:55 +0200

    Add files via upload

---
+++ Lecture12-2/Lecture12-2/Old Books.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Lecture12_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string book = Console.ReadLine();
+            int end = 0;
+
+            while (true)
+            {
+                string searching = Console.ReadLine();
+                if (book == searching)
+                {
+                    Console.WriteLine($"You checked {end} books and found it.");
+                    break;
+                }
+                else if (searching == "No More Books")
+                {
+                    Console.WriteLine("The book you search is not here!");
+                    Console.WriteLine($"You checked {end} books.");
+                    break;
+                }
+                
+                end++;
+            }
+
+        }
+    }
+}
