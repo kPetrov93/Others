346f093870217e22e2940719a8b682d0551b895f
Kristian Petrov <83224475+kPetrov93@users.noreply.github.com>
2021-11-15 11:45:49 +0200

    Add files via upload

---
+++ Lecture12-2/Vacation/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace Vacation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double vacationPrice = double.Parse(Console.ReadLine());
+            double money = double.Parse(Console.ReadLine());
+            int timesSpend = 0;
+            int days = 0;
+
+            while (true)
+            {
+                string action = Console.ReadLine();
+                double amount = double.Parse(Console.ReadLine());
+                days++;
+
+                if (action=="spend")
+                {
+                    money -= amount;
+                    if (money<=0)
+                    {
+                        money = 0;
+                    }
+
+                    timesSpend++;
+                    if (timesSpend>=5)
+                    {
+                        Console.WriteLine("You can't save the money.");
+                        Console.WriteLine(days);
+                        break;
+                    }
+                }
+                else if (action=="save")
+                {
+                    timesSpend = 0;
+                    money += amount;
+                    if (money>=vacationPrice)
+                    {
+                        Console.WriteLine($"You saved the money for {days} days.");
+                        break;
+                    }
+                }
+                
+                
+            }
+
+        }
+    }
+}
