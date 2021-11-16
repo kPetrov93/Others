13553880b9175b81f828cd056a5eb900b102b51b
Kristian Petrov <83224475+kPetrov93@users.noreply.github.com>
2021-11-16 10:08:29 +0200

    Add files via upload

---
+++ Lecture13/GraduationPt2/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace GraduationPt2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            double total = 0;
+            int grade = 0;
+            int rep = 0;
+
+            while (grade<12)
+            {
+                double mark = double.Parse(Console.ReadLine());
+                    grade++;
+                if (mark>=4)
+                {
+                    total += mark;
+                }
+                else if(mark<4)
+                {
+                    rep++;
+                    if (rep >= 2)
+                    {
+                        break;
+                    }
+                    continue;
+                }
+            }
+            double average = total / 12;
+            if (grade == 12)
+            {
+                Console.WriteLine($"{name} graduated. Average grade: {average:f2}");
+            }
+            else
+            {
+                Console.WriteLine($"{name} has been excluded at {grade-1} grade");
+            }
+        }
+    }
+}
