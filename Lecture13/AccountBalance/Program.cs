f2ddaf8d689dc6901c001afe6a272a7c62696aa4
Kristian Petrov <83224475+kPetrov93@users.noreply.github.com>
2021-11-16 10:07:45 +0200

    Add files via upload

---
+++ Lecture13/AccountBalance/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace AccountBalance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            double total = 0;
+            while (input != "NoMoreMoney")
+            {
+                double num = double.Parse(input);
+                if (num > 0)
+                {
+                    Console.WriteLine($"Increase: {num:f2}");
+                    total += num;
+                    input = Console.ReadLine();
+                }
+                else
+                {
+                    Console.WriteLine("Invalid operation!");
+                    break;
+                }
+            }
+            Console.WriteLine($"Total: {total:f2}");
+        }
+    }
+}
