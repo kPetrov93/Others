bdcede70252c334a6d0cee8d8b29ad14e70af71c
Kristian Petrov <83224475+kPetrov93@users.noreply.github.com>
2021-11-21 11:14:50 +0200

    Add files via upload

---
+++ Lecture30.01/Fun/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace fun
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            int c = int.Parse(Console.ReadLine());
+            double freeArea = a * b * c;
+
+            while (true)
+            {
+                string n = Console.ReadLine();
+                if (n == "Done")
+                {
+                    break;
+                }
+                int num = int.Parse(n);
+                freeArea -= num;
+                if (freeArea < 0)
+                {
+                    break;
+                }
+
+
+            }
+            if (freeArea < 0)
+            {
+                double area = Math.Abs(freeArea);
+                Console.WriteLine($"No more free space! You need {area} Cubic meters more.");
+
+            }
+            else
+            {
+                Console.WriteLine($"{freeArea} Cubic meters left.");    
+            }        
+        }
+    }
+}
