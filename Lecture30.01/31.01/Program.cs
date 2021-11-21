8dad78c8449a33a22ca3a3db016300a9fcd8223a
Kristian Petrov <83224475+kPetrov93@users.noreply.github.com>
2021-11-21 11:14:24 +0200

    Add files via upload

---
+++ Lecture30.01/31.01/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace _31._01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int max = int.MinValue;
+            int sum = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                int num = int.Parse(Console.ReadLine());
+                sum += num;
+                if (num > max)
+                {
+                    max = num;
+                }
+            }
+            int result = Math.Abs((sum - max) - max);
+
+            if (max==sum-max)
+            {
+                Console.WriteLine("Yes");
+                Console.WriteLine($"Sum = {max}");
+            }
+            else
+            {
+                Console.WriteLine("No");
+                Console.WriteLine($"Diff = {result}");
+            }
+
+        }
+    }
+}
