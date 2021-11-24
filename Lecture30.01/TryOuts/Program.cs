98cdd5923aded741dbef88a5a1cc2730371d1647
Kristian Petrov <83224475+kPetrov93@users.noreply.github.com>
2021-11-24 09:53:41 +0200

    Add files via upload

---
+++ Lecture30.01/TryOuts/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace dawd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double n = double.Parse(Console.ReadLine());
+            double salary = double.Parse(Console.ReadLine());
+            double fine = 0;
+            for (int i = 0; i < n; i++)
+            {
+                string site = Console.ReadLine();
+
+                if (site == "Facebook")
+                {
+                    fine += 150;
+                }
+                else if (site == "Instagram")
+                {
+                    fine += 100;
+                }
+                else if (site == "Reddit")
+                {
+                    fine += 50;
+                }
+                if (salary - fine <= 0)
+                {
+                    Console.WriteLine("You have lost your salary.");
+                    break;
+                }
+            }
+            if (salary - fine > 0)
+            {
+                Console.WriteLine(salary-fine);
+                
+            }
+
+
+        }
+    }
+}
