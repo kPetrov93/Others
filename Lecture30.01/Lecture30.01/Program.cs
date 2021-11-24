0279eec995b1210843166dbd477f967fbbf504a6
Kristian Petrov <83224475+kPetrov93@users.noreply.github.com>
2021-11-24 09:53:26 +0200

    Add files via upload

---
+++ Lecture30.01/Lecture30.01/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Lecture30._01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+           double even = 0;
+            double odd = 0;
+            for (int i = 0; i < n; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    int num1 = int.Parse(Console.ReadLine());
+                   
+                    even += num1;
+                }
+                else
+                {
+                    int num1 = int.Parse(Console.ReadLine());
+                    ;
+                    odd += num1;
+                }
+            }
+            double diff = Math.Abs(even - odd);
+            if (even == odd)
+            {
+                Console.WriteLine($"Yes");
+                Console.WriteLine($"Sum = {even}");
+            }
+            else
+            {
+                Console.WriteLine($"No");
+                Console.WriteLine($"Diff = {diff}");
+            }
+        }
+    }
+}
