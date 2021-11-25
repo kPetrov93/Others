ea79600ad3c2905491e46ad60c8f3b6c33f6bcc0
Kristian Petrov <83224475+kPetrov93@users.noreply.github.com>
2021-11-25 09:38:02 +0200

    Add files via upload

---
+++ ListsExercise/CardsGame/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CardsGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> firstPlayer = Console.ReadLine().Split().Select(int.Parse).ToList();
+            List<int> secondPlayer = Console.ReadLine().Split().Select(int.Parse).ToList();
+
+            while (firstPlayer.Count>0&&secondPlayer.Count>0)
+            {
+                if (firstPlayer.Count >= secondPlayer.Count)
+                {
+                    
+                   if (firstPlayer[0] > secondPlayer[0])
+                   {
+                       firstPlayer.Add(firstPlayer[0]);
+                       firstPlayer.Remove(firstPlayer[0]);
+                       firstPlayer.Add(secondPlayer[0]);
+                       secondPlayer.Remove(secondPlayer[0]);
+                   }
+                   else if (firstPlayer[0] == secondPlayer[0])
+                   {
+                       firstPlayer.Remove(firstPlayer[0]);
+                       secondPlayer.Remove(secondPlayer[0]);
+                   }
+                   else if (firstPlayer[0] < secondPlayer[0])
+                   {
+                       secondPlayer.Add(secondPlayer[0]);
+                       secondPlayer.Remove(secondPlayer[0]);
+                       secondPlayer.Add(firstPlayer[0]);
+                       firstPlayer.Remove(firstPlayer[0]);
+                   }
+                    
+                }
+                else
+                {
+                    
+                    if (firstPlayer[0] > secondPlayer[0])
+                    {
+                        firstPlayer.Add(firstPlayer[0]);
+                        firstPlayer.Remove(firstPlayer[0]);
+                        firstPlayer.Add(secondPlayer[0]);
+                        secondPlayer.Remove(secondPlayer[0]);
+                    }
+                    else if (firstPlayer[0] == secondPlayer[0])
+                    {
+                        firstPlayer.Remove(firstPlayer[0]);
+                        secondPlayer.Remove(secondPlayer[0]);
+                    }
+                    else if (firstPlayer[0] < secondPlayer[0])
+                    {
+                        secondPlayer.Add(secondPlayer[0]);
+                        secondPlayer.Remove(secondPlayer[0]);
+                        secondPlayer.Add(firstPlayer[0]);
+                        firstPlayer.Remove(firstPlayer[0]);
+                    }
+                }
+
+            }
+            if (firstPlayer.Count > secondPlayer.Count)
+            {
+                Console.WriteLine($"First player wins! Sum: {firstPlayer.Sum()}");
+            }
+            else
+            {
+                Console.WriteLine($"Second player wins! Sum: {secondPlayer.Sum()}");
+            }
+        }
+    }
+}
