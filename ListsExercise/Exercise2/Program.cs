02dc88ac80fd0a9545be422de0820dadfa13e16f
Kristian Petrov <83224475+kPetrov93@users.noreply.github.com>
2021-11-26 11:47:13 +0200

    Add files via upload

---
+++ ListsExercise/Exercise2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Exercise2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> input = Console.ReadLine().Split('|').Reverse().ToList();
+            List<int> numbers = new List<int>();
+            foreach (var str in input)
+            {
+                numbers.AddRange(str.Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                                    .Select(int.Parse)
+                                    .ToList()
+                                     );
+            }
+
+            Console.WriteLine(string.Join(" ",numbers));
+        }
+    }
+}
