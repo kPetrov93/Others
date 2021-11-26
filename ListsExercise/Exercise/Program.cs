b069ec527b16e19198517dd1fb849f7d1161f459
Kristian Petrov <83224475+kPetrov93@users.noreply.github.com>
2021-11-26 11:46:47 +0200

    Add files via upload

---
+++ ListsExercise/Exercise/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Exercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> input = Console.ReadLine().Split().Select(int.Parse).ToList();
+            int[] num = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int bomb = num[0];
+            int power = num[1];
+
+            for (int i = 0; i < input.Count; i++)
+            {
+                if (input[i] == bomb)
+                {
+                    for (int j = i - power; j <= i + power; j++)
+                    {
+                        if (j < 0 || j >= input.Count)
+                        {
+                            continue;
+                        }
+                        
+                       input[j] = 0;
+                        
+                    }
+                }
+            }
+            Console.WriteLine(input.Sum());
+
+        }
+    }
+}
