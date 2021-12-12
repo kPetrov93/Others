d289e338b852cf82ae8b0d86b77171def2dc6acd
Kristian Petrov <83224475+kPetrov93@users.noreply.github.com>
2021-12-12 10:27:51 +0200

    Add files via upload

---
+++ ProblemOne/areaOfFigures/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace areaOfFigures
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string figure = Console.ReadLine();
+
+            if (figure == "square")
+            {
+                double side = double.Parse(Console.ReadLine());
+                double area = side * side;
+                Console.WriteLine($"{area:f3}");
+            }
+
+            else if (figure== "rectangle")
+            {
+                double side = double.Parse(Console.ReadLine());
+                double side2 = double.Parse(Console.ReadLine());
+                double area = side * side2;
+                Console.WriteLine($"{area:f3}");
+            }
+
+            else if (figure == "circle")
+            {
+                double side = double.Parse(Console.ReadLine());
+                double area = side * side * Math.PI;
+               
+                Console.WriteLine($"{area:f3}");
+            }
+
+            else if (figure == "triangle")
+            {
+                double side = double.Parse(Console.ReadLine());
+                double side2 = double.Parse(Console.ReadLine());
+                double area = (side * side2) / 2;
+                Console.WriteLine($"{area:f3}");
+            }
+
+
+
+        }
+    }
+}
