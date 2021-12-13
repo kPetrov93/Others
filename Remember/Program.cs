d9a38e1478bb754373c1bf80161f46ca8243f6ce
Kristian Petrov <83224475+kPetrov93@users.noreply.github.com>
2021-12-13 10:52:35 +0200

    Add files via upload

---
+++ Remember/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Remember
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
