91e7d6ea3aec6ef76e709016548a16637919aa47
Kristian Petrov <83224475+kPetrov93@users.noreply.github.com>
2021-12-16 11:06:00 +0200

    Add files via upload

---
+++ StockMarket/Investor.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace StockMarket
+{
+    public class Investor
+    {
+        private List<Stock> Portfolio;
+
+        public Investor(string fullName, string emailAddress, decimal moneyToInvest, string brokerName)
+        {
+            FullName = fullName;
+            EmailAddress = emailAddress;
+            MoneyToInvest = moneyToInvest;
+            BrokerName = brokerName;
+            Portfolio = new List<Stock>();
+        }
+
+        public string FullName { get; set; }
+
+        public string EmailAddress { get; set; }
+
+        public decimal MoneyToInvest { get; set; }
+
+        public string BrokerName { get; set; }
+
+        public int Count { get { return Portfolio.Count; } }
+
+        public void BuyStock(Stock stock)
+        {
+            if (stock.MarketCapitalization > 10000 && MoneyToInvest > stock.PricePerShare)
+            {
+                Portfolio.Add(stock);
+                MoneyToInvest -= stock.PricePerShare;
+            }
+        }
+
+        public string SellStock(string companyName, decimal sellPrice)
+        {
+            var company = Portfolio.FirstOrDefault(x => x.CompanyName == companyName);
+
+            if (company == null)
+            {
+                return $"{companyName} does not exist.";
+            }
+            else if (sellPrice < company.PricePerShare)
+            {
+                return $"Cannot sell {companyName}.";
+            }
+            else
+            {
+                Portfolio.Remove(company);
+                MoneyToInvest += sellPrice;
+                return $"{companyName} was sold.";
+            }
+        }
+
+        public Stock FindStock(string companyName)
+        {
+            return Portfolio.FirstOrDefault(x => x.CompanyName == companyName);
+        }
+
+        public Stock FindBiggestCompany()
+        {
+            return Portfolio.OrderByDescending(x => x.MarketCapitalization).FirstOrDefault();
+        }
+
+        public string InvestorInformation()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"The investor {FullName} with a broker {BrokerName} has stocks:");
+            foreach (var item in Portfolio)
+            {
+                sb.AppendLine($"{item}");
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
