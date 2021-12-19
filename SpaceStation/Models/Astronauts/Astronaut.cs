5ffc4f342da37d3b8beb0435e2bb553873f9a807
Kristian Petrov <83224475+kPetrov93@users.noreply.github.com>
2021-12-19 10:12:22 +0200

    Add files via upload

---
+++ SpaceStation/Models/Astronauts/Astronaut.cs
@@ -0,0 +1,69 @@
+﻿using SpaceStation.Models.Astronauts.Contracts;
+using SpaceStation.Models.Bags;
+using SpaceStation.Models.Bags.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SpaceStation.Models
+{
+    public abstract class Astronaut : IAstronaut
+    {
+        private string name;
+        private double oxygen;
+
+        public Astronaut(string name, double oxygen)
+        {
+            Name = name;
+            Oxygen = oxygen;
+
+            Bag = new Backpack();
+        }
+
+        public string Name
+        {
+            get => name;
+            private set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentNullException(nameof(Name),"Astronaut name cannot be null or empty.");
+                }
+
+                name = value;
+            }
+
+        }
+
+        public double Oxygen
+        {
+            get => oxygen;
+            protected set
+            {
+                if (value<0)
+                {
+                    throw new ArgumentException("Cannot create Astronaut with negative oxygen!");
+                }
+
+                oxygen = value;
+            }
+
+        }
+
+        public bool CanBreath => Oxygen > 0;
+
+        public IBag Bag { get; }
+
+        public virtual void Breath()
+        {
+            if (Oxygen - 10 < 0)
+            {
+                Oxygen = 0;
+            }
+            else
+            {
+                Oxygen -= 10;
+            }
+        }
+    }
+}
