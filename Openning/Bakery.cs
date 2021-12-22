135354d7a666a722ca3d8865e576924587088426
Kristian Petrov <83224475+kPetrov93@users.noreply.github.com>
2021-12-22 09:47:50 +0200

    Add files via upload

---
+++ Openning/Bakery.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BakeryOpenning
+{
+    public class Bakery
+    {
+        private List<Employee> data;
+
+        public Bakery(string name, int capacity)
+        {
+            Name = name;
+            Capacity = capacity;
+            data = new List<Employee>();
+        }
+
+        public string Name { get; set; }
+
+        public int Capacity { get; set; }
+
+        public int Count { get { return data.Count; } }
+
+        public void Add(Employee employee)
+        {
+            if (data.Count < Capacity)
+            {
+                data.Add(employee);
+            }
+        }
+
+        public bool Remove(string name)
+        {
+            var employee = data.FirstOrDefault(x => x.Name == name);
+            return data.Remove(employee);
+        }
+
+        public Employee GetOldestEmployee()
+        {
+            return data.OrderByDescending(x => x.Age).FirstOrDefault();
+        }
+
+        public Employee GetEmployee(string name)
+        {
+            return data.FirstOrDefault(x => x.Name == name);
+        }
+
+        public string Report()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"Employees working at Bakery {Name}:");
+            foreach (var person in data)
+            {
+                sb.AppendLine($"{person}");
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
